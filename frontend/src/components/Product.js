140280464a8be8f75575a41721348a36f30ddd43
Novailoveyou <novailoveoux@gmail.com>
2021-01-10 16:56:29 +0300

    HomeScreen Product Listing

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 export const Product = ({ product }) => {
   return (
@@ -18,12 +19,10 @@ export const Product = ({ product }) => {
         </a>
 
         <Card.Text as='div'>
-          <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
         </Card.Text>
 
-        <Card.Text as='h3'>${product.price}</Card.Text>
+        <Card.Text as='h3' className='product__price'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
